107b40468ceaaed4dfa3646b9c256efdfa6aeac6
IvanShpotenko <Ivan.shpotenko@gmail.com>
2017-03-17 11:05:31 +0200

    Update chunks.py

--- chunks.py
+++ chunks.py
@@ -9,8 +9,8 @@ def chunks(iterable: iter, n: int) -> iter:
     Last chunk will be sized as is and will not be padded with any value.
     ABCDEFGH -> ABC, DEF, GH
 
-    >>> for item in chunks(range(25), n=7):
-    ...     print(list(item))
+    >>> for chunk in chunks(range(25), n=7):
+    ...     print(list(chunk))
     [0, 1, 2, 3, 4, 5, 6]
     [7, 8, 9, 10, 11, 12, 13]
     [14, 15, 16, 17, 18, 19, 20]
